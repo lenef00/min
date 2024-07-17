13f4d0fa27736ba7fa9a4c176e71d8ced400d065
lenef00 <lenef3315@gmail.com>
2024-07-17 14:11:57 +0900

    0

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         },
 
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        effect: "fade", 
 
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
             el: '.visual .paging', /* 해당 요소의 class명 */
@@ -23,7 +24,7 @@ $(document).ready(function(){
         },
 
     });
-});
+
       /* 재생 기능 */
 
     /* 
@@ -41,10 +42,29 @@ $(document).ready(function(){
     $('.visual .btn_wrap button.btn_stop').on('click', function(){
         visual_swiper.autoplay.stop();  /* 일시정지 기능 */
         $(this).hide()
-        $('visual .btn_wrap button.btn_stop').show() //보이기
+        $('.visual .btn_wrap button.btn_play').show() //보이기
     })    
     $('.visual .btn_wrap button.btn_play').on('click', function(){
-        visual_swiper.autoplay.start();  /* 일시정지 기능 */
+        visual_swiper.autoplay.start();  /* 재생기능 */
         $(this).hide()
-        $('visual .btn_wrap button.btn_play').show() //보이기    
+        $('.visual .btn_wrap button.btn_stop').show() //보이기    
     })
+
+    /*
+        ---pc일 때   
+        .biz .list ul li 에 마우스를 올리면
+        마우스를 올린 li는 on클래스 추가
+        마우스를 올리지 않은 li는 off클래스 추가
+    */
+   $('.biz .list ul li').on('mouseenter', function(){
+        $('.biz .list ul li').removeClass('on')
+        $('.biz .list ul li').addClass('off')
+        $(this).removeClass('off')
+        $(this).addClass('on')
+   })
+   $('.biz .list').on('mouseleave', function(){
+        $('.biz .list ul li').removeClass('on')
+        $('.biz .list ul li').removeClass('off')
+    })
+});    
+
