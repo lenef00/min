cad7754d121a410ac21cf7fa31e0f8671ca59dea
lenef00 <lenef3315@gmail.com>
2024-08-30 18:01:49 +0900

    0830

--- 00helloaqua/js/main.js
+++ 00helloaqua/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   const visual_swiper = new Swiper('.visual .swiper', {
 
     autoplay: {
-      delay: 5000,
+      delay: 3200,
       disableOnInteraction: true,
     },
     loop: true, 
@@ -12,18 +12,15 @@ $(document).ready(function(){
         },
   });
 
-  const new_swiper = new Swiper('.new .swiper', { /* 팝업을 감싼는 요소의 class명 */
-    slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
-    spaceBetween: 24, /* li와 li사이 - 제일 작은 여백 */
+  const new_swiper = new Swiper('.new .swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 24,
     breakpoints: {
-      640: {  /* 640px 이상이 되면 적용 */
-        spaceBetween: 30, 
-      },
-      1024: {  /* 1024px 이상이 되면 적용 */
-        spaceBetween: 40,
+      1024: { 
+        spaceBetween: 24,
       },
     },
-    centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+    centeredSlides: false, 
     loop: true,
 
     navigation: {
